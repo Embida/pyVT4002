ede262fe410236156298620690b87992a2c81013
Joar Gjersund <joar.gjersund@embida.no>
2019-03-05 10:40:25 +0100

    setup pypip

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='pyVT4002',
+     version='0.1.1',
+     scripts=['pyVT4002'] ,
+     author="Embida AS",
+     author_email="post@embida.no",
+     description="A driver for vt4002 Vötsch climate chamber",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Embida/pyVT4002",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+         "Operating System :: OS Independent",
+     ],
+ )
